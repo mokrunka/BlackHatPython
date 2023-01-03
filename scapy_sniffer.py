2ac270a53b36bd5ae0aae117e04627c313a3def8
mokrunka <hunker.engineer@gmail.com>
2023-01-02 21:29:02 -0800

    refactored scapy sniffer

--- scapy_sniffer.py
+++ scapy_sniffer.py
@@ -6,12 +6,13 @@ import threading
 
 SUBNET = '192.168.10.0/24'
 MESSAGE = '^_^'
-hosts = []
+HOSTS = []
 
 
 def udp_sender():
     # blasts udp packets into the network to solicit responses
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sender:
+        print(f'Sending UDP packets to {SUBNET}...')
         for ip in ipaddress.ip_network(SUBNET).hosts():
             # send our test message out to port 65212 on the destination
             sender.sendto(bytes(MESSAGE, 'utf8'), (str(ip), 65212))
@@ -21,31 +22,23 @@ def packet_callback(packet):
     # print(packet.show())
     # print(packet.summary())
     # print(packet.command())
+    # type 3 / code 3 means destination and port unreachable, but indicates a host is likely up
     if packet[ICMP].type == 3 and packet[ICMP].code == 3:
-        print(packet.command())
-        print(packet[IP].src)
+        if packet[IP].src not in HOSTS:
+            HOSTS.append(packet[IP].src)
+    print(HOSTS)
+        # this prints the command that could be used to generate the response packets in scapy (for reference)
+        # print(packet.command())
 
 
 def main(target_ip):
     while True:
-        try:
-            # filter
-            p_filter = f'dst {target_ip} or src {target_ip} and icmp'
-            # pack is a list of packets
-            pack = sniff(filter=p_filter, prn=packet_callback, count=2)
-            for p in pack:
-                print(p.sniffed_on)
-        except KeyboardInterrupt:
-            break
-        finally:
-            host_discovery_list(pack)
-
-
-def host_discovery_list(pack):
-    for p in pack:
-        if p[IP].src not in hosts:
-            hosts.append(p[IP].src)
-    print(hosts)
+        # filter
+        p_filter = f'dst {target_ip} or src {target_ip} and icmp'
+        # pack is a list of packets
+        pack = sniff(filter=p_filter, prn=packet_callback, count=2)
+        # for p in pack:
+            # print(p.sniffed_on)
 
 
 if __name__ == "__main__":
